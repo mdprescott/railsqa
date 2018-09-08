23152d752ad06f8d8d53a1d0541ff439a1bc576e
deploy <deploy@test>
2018-09-08 20:03:23 +0000

    Older version - pre cap

---
+++ qa/config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/home/deploy/railsqa/qa"
+pid "/home/deploy/railsqa/qa/pids/unicorn.pid"
+stderr_path "/home/deploy/railsqa/qa/unicorn/unicorn.log"
+stdout_path "/home/deploy/railsqa/qa/unicorn/unicorn.log"
+
+listen "/tmp/unicorn.qa.sock"
+worker_processes 2
+timeout 30
